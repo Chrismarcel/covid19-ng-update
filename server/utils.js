9a39348cf1a7f422efacb10e9940ae00142ded05
Chrismarcel <cmarcelj@gmail.com>
2021-06-27 00:08:05 +0100

    refactor

--- server/utils.js
+++ server/utils.js
@@ -1,13 +1,13 @@
 const COLOR_BANDS = {
-  DEFAULT: '#F88379',
-  LESS_THAN_11: '#FF0800',
-  LESS_THAN_51: '#EB8967',
-  LESS_THAN_101: '#E66C41',
-  LESS_THAN_501: '#D0562B',
-  LESS_THAN_1001: '#B31B1B',
-  LESS_THAN_3001: '#682B16',
-  LESS_THAN_5001: '#8A0707',
-  GREATER_THAN_5000: '#5B342E',
+  DEFAULT: { color: '#F88379', text: 'No cases' },
+  LESS_THAN_11: { color: '#FF0800', text: '1 - 10' },
+  LESS_THAN_51: { color: '#EB8967', text: '11 - 50' },
+  LESS_THAN_101: { color: '#E66C41', text: '51 - 100' },
+  LESS_THAN_501: { color: '#D0562B', text: '101 - 500' },
+  LESS_THAN_1001: { color: '#B31B1B', text: '501 - 1000' },
+  LESS_THAN_3001: { color: '#682B16', text: '1001 - 3000' },
+  LESS_THAN_5001: { color: '#8A0707', text: '3001 - 5000' },
+  GREATER_THAN_5000: { color: '#5B342E', text: '5000+' },
 }
 
 const generateChloropheth = (numCases) => {
@@ -16,34 +16,34 @@ const generateChloropheth = (numCases) => {
   let color
   switch (true) {
     case numCases < 1:
-      color = COLOR_BANDS.DEFAULT
+      color = COLOR_BANDS.DEFAULT.color
       break
     case numCases < 11:
-      color = COLOR_BANDS.LESS_THAN_11
+      color = COLOR_BANDS.LESS_THAN_11.color
       break
     case numCases < 51:
-      color = COLOR_BANDS.LESS_THAN_51
+      color = COLOR_BANDS.LESS_THAN_51.color
       break
     case numCases < 101:
-      color = COLOR_BANDS.LESS_THAN_101
+      color = COLOR_BANDS.LESS_THAN_101.color
       break
     case numCases < 501:
-      color = COLOR_BANDS.LESS_THAN_501
+      color = COLOR_BANDS.LESS_THAN_501.color
       break
     case numCases < 1001:
-      color = COLOR_BANDS.LESS_THAN_1001
+      color = COLOR_BANDS.LESS_THAN_1001.color
       break
     case numCases < 3001:
-      color = COLOR_BANDS.LESS_THAN_3001
+      color = COLOR_BANDS.LESS_THAN_3001.color
       break
     case numCases < 5001:
-      color = COLOR_BANDS.LESS_THAN_5001
+      color = COLOR_BANDS.LESS_THAN_5001.color
       break
     case numCases > 5000:
-      color = COLOR_BANDS.GREATER_THAN_5000
+      color = COLOR_BANDS.GREATER_THAN_5000.color
       break
     default:
-      color = COLOR_BANDS.DEFAULT
+      color = COLOR_BANDS.DEFAULT.color
       break
   }
 
@@ -102,9 +102,9 @@ const formatNumber = (number) => {
 
 const isChildNode = (parentNode, childNode) => {
   if ('contains' in parentNode) {
-    return parentNode.contains(childNode);
+    return parentNode.contains(childNode)
   } else {
-    return parentNode.compareDocumentPosition(childNode) % 16 !== 4;
+    return parentNode.compareDocumentPosition(childNode) % 16 !== 4
   }
 }
 
@@ -116,5 +116,5 @@ module.exports = {
   COLOR_BANDS,
   allStates,
   formatNumber,
-  isChildNode
+  isChildNode,
 }
--- src/components/MapLegends.js
+++ src/components/MapLegends.js
@@ -1,44 +1,16 @@
 import React from 'react'
 import { COLOR_BANDS } from '../../server/utils'
 
+const legends = Object.values(COLOR_BANDS)
+
 const MapLegends = () => (
   <section className="map-legends-wrapper">
-    <div className="map-legend">
-      <div style={{ background: COLOR_BANDS.DEFAULT }} className="map-legend-color"></div>
-      <p className="map-legend-text">No cases</p>
-    </div>
-    <div className="map-legend">
-      <div style={{ background: COLOR_BANDS.LESS_THAN_11 }} className="map-legend-color"></div>
-      <p className="map-legend-text">1 - 10</p>
-    </div>
-    <div className="map-legend">
-      <div style={{ background: COLOR_BANDS.LESS_THAN_51 }} className="map-legend-color"></div>
-      <p className="map-legend-text">11 - 50</p>
-    </div>
-    <div className="map-legend">
-      <div style={{ background: COLOR_BANDS.LESS_THAN_101 }} className="map-legend-color"></div>
-      <p className="map-legend-text">51 - 100</p>
-    </div>
-    <div className="map-legend">
-      <div style={{ background: COLOR_BANDS.LESS_THAN_501 }} className="map-legend-color"></div>
-      <p className="map-legend-text">101 - 500</p>
-    </div>
-    <div className="map-legend">
-      <div style={{ background: COLOR_BANDS.LESS_THAN_1001 }} className="map-legend-color"></div>
-      <p className="map-legend-text">501 - 1000</p>
-    </div>
-    <div className="map-legend">
-      <div style={{ background: COLOR_BANDS.LESS_THAN_3001 }} className="map-legend-color"></div>
-      <p className="map-legend-text">1001 - 3000</p>
-    </div>
-    <div className="map-legend">
-      <div style={{ background: COLOR_BANDS.LESS_THAN_5001 }} className="map-legend-color"></div>
-      <p className="map-legend-text">3001 - 5000</p>
-    </div>
-    <div className="map-legend">
-      <div style={{ background: COLOR_BANDS.GREATER_THAN_5000 }} className="map-legend-color"></div>
-      <p className="map-legend-text"> 5001+</p>
-    </div>
+    {legends.map((legend) => (
+      <div className="map-legend">
+        <div style={{ background: legend.color }} className="map-legend-color"></div>
+        <p className="map-legend-text">{legend.text}</p>
+      </div>
+    ))}
   </section>
 )
 
